9367f3d7d5171240991635653408a2cfc392d42c
Bryan <d58526@gmail.com>
2017-11-22 17:52:53 +0800

    modify

--- RDRockLife/Controllers/LineController.cs
+++ RDRockLife/Controllers/LineController.cs
@@ -30,18 +30,24 @@ namespace RDRockLife.Controllers
                 var receivedMessage = isRock.LineBot.Utility.Parsing(postData);
                 replyToken = receivedMessage.events[0].replyToken;
                 var message = GetMessage(receivedMessage);
-                isRock.LineBot.Utility.ReplyMessage(replyToken, message, this._channelToken);
+                if (!string.IsNullOrEmpty(message))
+                {
+                    isRock.LineBot.Utility.ReplyMessage(replyToken, message, this._channelToken);
+                }
                 return Ok();
             }
             catch (Exception ex)
             {
-                isRock.LineBot.Utility.ReplyMessage(replyToken, ex.Message, this._channelToken);
+                isRock.LineBot.Utility.ReplyMessage(
+                    replyToken,
+                    Newtonsoft.Json.JsonConvert.SerializeObject(ex),
+                    this._channelToken);
                 return Ok();
             }
         }
 
         [HttpGet]
-        public IHttpActionResult Get(string stockId)
+        public IHttpActionResult Get(int stockId)
         {
             try
             {
@@ -56,16 +62,54 @@ namespace RDRockLife.Controllers
 
         private string GetMessage(ReceievedMessage receivedMessage)
         {
+            if (receivedMessage.events[0].type == "join")
+            {
+                return "歡迎來到布萊恩RD滾石人生頻道";
+            }
+
+            //if (receivedMessage.events[0].type == "message" && receivedMessage.events[0].source.userId
+            //    == "Ucb61a0292a1972e531f8bd123a7dca02")
+            //{
+            //    return "林貝珊再吵打你屁股";
+            //}
+            //if (receivedMessage.events[0].type == "message" && receivedMessage.events[0].source.userId == "U142ea503c7835a515b3ad34db5506f1f")
+            //{
+            //    return "謝宗明不要再鴨講了";
+            //}
+
+            //if (receivedMessage.events[0].type == "message" && receivedMessage.events[0].source.userId == "Ud0b4e4ce86825b1621b960741726223a")
+            //{
+            //    return "阿信就是胡瓜";
+            //}
+
+            //if (receivedMessage.events[0].type == "message" && receivedMessage.events[0].source.userId == "Ub3f1e20be96bd82b11192c613768423c")
+            //{
+            //    return "劉醬瓜不要再遲到了";
+            //}
+
             var messageText = receivedMessage.events[0].message.text;
+
+            //if (receivedMessage.events[0].type == "message" && messageText.Contains("五月天"))
+            //{
+            //    return "浪費錢";
+            //}
+            //if (receivedMessage.events[0].type == "message" && messageText.Contains("阿信"))
+            //{
+            //    return "胡瓜";
+            //}
             if (receivedMessage.events[0].type == "message" && messageText.Contains("股票"))
             {
-                var stockId = messageText.Replace("股票", string.Empty).Trim();
-                return GetStock(stockId);
+                int stockId = 0;
+                bool isStockId = int.TryParse(messageText.Replace("股票", string.Empty).Trim(), out stockId);
+                if (isStockId)
+                {
+                    return GetStock(stockId);
+                }
             }
-            return "找不到";
+            return string.Empty;
         }
 
-        private string GetStock(string stockId)
+        private string GetStock(int stockId)
         {
             //指定來源網頁
             WebClient url = new WebClient();
